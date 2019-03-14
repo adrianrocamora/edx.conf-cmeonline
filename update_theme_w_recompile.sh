ccf544180839f1cf599e89678df000acf44de44d
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-03-13 19:25:30 -0600

    Updated theme scripts

--- update_theme_w_recompile.sh
+++ update_theme_w_recompile.sh
@@ -3,6 +3,10 @@ source /edx/app/edxapp/edxapp_env
 rm -rf /edx/app/edxapp/edx-platform/themes/cmeonline-theme
 mkdir /edx/app/edxapp/edx-platform/themes/cmeonline-theme
 cp -r /home/ubuntu/edx.conf-cmeonline/themes/cmeonline-theme/* /edx/app/edxapp/edx-platform/themes/cmeonline-theme
+
+cd /edx/app/edxapp/edx-platform
+paver update_assets lms --settings=aws | tee ~/custom-theme-lms.log
+paver update_assets cms --settings=aws | tee ~/custom-theme-cms.log
 EOF
 
 /edx/bin/supervisorctl status
