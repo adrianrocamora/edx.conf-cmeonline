f2812bde03c5a88a0f0e970141657e17c870ac30
Lawrence McDaniel <lpm0073@gmail.com>
2019-04-10 10:17:15 -0500

    configure oauth installer

--- home/ubuntu/edx.install-oauth.sh
+++ home/ubuntu/edx.install-oauth.sh
@@ -14,14 +14,17 @@
 # reference:  https://github.com/QueriumCorp/openstax-oauth-backend
 #---------------------------------------------------------
 
-sudo rm -r /edx/app/edxapp/edx-platform/xblocks/openstax-oauth-backend
+#sudo rm -r /edx/app/edxapp/edx-platform/xblocks/openstax-oauth-backend
 
 sudo -H -u edxapp bash << EOF
 source /edx/app/edxapp/edxapp_env
-cd /edx/app/edxapp/venvs/edxapp/.ssh
-/edx/bin/pip.edxapp uninstall -y openstax-oauth-backend
+# cd /edx/app/edxapp/venvs/edxapp/.ssh
 
-git clone git@github-openstaxoauth:QueriumCorp/openstax-oauth-backend.git /edx/app/edxapp/edx-platform/xblocks/openstax-oauth-backend
-/edx/bin/pip.edxapp install -e /edx/app/edxapp/edx-platform/xblocks/openstax-oauth-backend/
+# public repo installation
+/edx/bin/pip.edxapp install --upgrade git+https://github.com/cmeonline/edx-oauth.git
+
+# private repo example
+# git clone git@github-openstaxoauth:QueriumCorp/openstax-oauth-backend.git /edx/app/edxapp/edx-platform/xblocks/openstax-oauth-backend
+# /edx/bin/pip.edxapp install -e /edx/app/edxapp/edx-platform/xblocks/openstax-oauth-backend/
 
 EOF
