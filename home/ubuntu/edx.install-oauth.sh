a4b15e139a9dc13e4156d3d0188982bae1e516ff
Lawrence McDaniel <lpm0073@gmail.com>
2019-04-10 10:20:37 -0500

    configure oauth installer

--- home/ubuntu/edx.install-oauth.sh
+++ home/ubuntu/edx.install-oauth.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 #---------------------------------------------------------
-# written by: Jim Van Riper
-#             jim@querium.com
-#
-#             Lawrence McDaniel
+# written by: Lawrence McDaniel
 #             lpm0073@gmail.com
 #
-# date:       Oct-2018
+# date:       April 2019
 #
-# usage:      install or reinstall openstax oauth backend
+# usage:      install or reinstall NYSPMA oauth backend
 #
 #
-# reference:  https://github.com/QueriumCorp/openstax-oauth-backend
+# reference:  https://github.com/cmeonline/edx-oauth
 #---------------------------------------------------------
 
-#sudo rm -r /edx/app/edxapp/edx-platform/xblocks/openstax-oauth-backend
 
+# install from public repo. force update if its already installed.
 sudo -H -u edxapp bash << EOF
 source /edx/app/edxapp/edxapp_env
-# cd /edx/app/edxapp/venvs/edxapp/.ssh
 
-# public repo installation
 /edx/bin/pip.edxapp install --upgrade git+https://github.com/cmeonline/edx-oauth.git
-
-# private repo example
-# git clone git@github-openstaxoauth:QueriumCorp/openstax-oauth-backend.git /edx/app/edxapp/edx-platform/xblocks/openstax-oauth-backend
-# /edx/bin/pip.edxapp install -e /edx/app/edxapp/edx-platform/xblocks/openstax-oauth-backend/
 
 EOF
